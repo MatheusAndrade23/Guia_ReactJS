27e179982f86331426211aebd613ca7de9626562
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-02-22 20:21:36 -0300

    PageNotFound

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/layouts/Header.jsx';
 import Intro from './components/pages/Intro.jsx';
 import Hooks from './components/pages/Hooks.jsx';
 import Js from './components/pages/Js.jsx';
+import PageNotFound from './components/pages/PageNotFound.jsx';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/intro' element={<Intro />}/>
           <Route path='/hooks' element={<Hooks />}/>
           <Route path='/js' element={<Js />}/>
+          <Route path='*' element={<PageNotFound />}/>
         </Routes>
     </Router>
   )
---
+++ src/components/pages/PageNotFound.jsx
@@ -0,0 +1,10 @@
+function PageNotFound() {
+    return(
+        <div className="container404">
+            <h1>Erro 404</h1>
+            <p>Essa página não existe!</p>
+        </div>
+    )
+};
+
+export default PageNotFound;

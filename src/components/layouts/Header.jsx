89dc4fa540d0d058663ee50e3cfc7befd8deb3b8
MatheusAndrade23 <matheusandrade.ma2003@gmail.com>
2022-02-23 19:55:48 -0300

    Responsivo

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -7,14 +7,17 @@ function Header(){
                 <img src={logo} alt="logo_react"/>
                 <p>Guia ReactJS</p>
             </a>
-            <ul>
+            <ul id='menu'>
                 <li><a href='/intro'>Introdução</a></li>
                 <li><a href='/hooks'>React Hooks</a></li>
                 <li><a href='/js'>JavaScript</a></li>
                 <li><a href='https://github.com/MatheusAndrade23/Guia_ReactJS'>GitHub</a></li>
             </ul>
+            <button>Menu
+                <span id='hamburguer'></span>
+            </button>
         </header>
     )
-}
+};
 
 export default Header;
